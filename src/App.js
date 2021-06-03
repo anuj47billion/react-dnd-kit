47f049c66dda6fdda3d050869b7d18110a00f198
anuj47billion <anuj.gangrade@47billion.com>
2021-06-03 11:19:13 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
   function handleDragEnd(event) {
     const { active, over } = event;
 
-    if (active.id !== over.id) {
+    if (over && active.id !== over.id) {
       setItems((items) => {
         const oldIndex = items.indexOf(active.id);
         const newIndex = items.indexOf(over.id);
